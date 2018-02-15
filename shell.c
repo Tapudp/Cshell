3421b6b6262d290b25e89577b811e9cf7d0d5c19
Tapudp <divyeshpar2496@gmail.com>
2018-02-15 21:31:27 +0530

    first commit and implemented void lsh_loop, char *lsh_read_line and char **lsh_split_line with pointers where as before it was with arrays.

---
+++ shell.c
@@ -0,0 +1,98 @@
+int main(int argc, char** argv){
+    // Load config files if any.
+
+    // Run command loop
+    lsh_loop();
+
+    //perform any shutdown cleanup  
+}
+
+void lsh_loop(void){
+    char *line;
+    char **args;
+    int status;
+
+    do{
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+    }
+    while(status);
+}
+
+#define LSH_RL_BUFSIZE 1024
+char *lsh_read_line(void)
+{
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if(!buffer) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1){
+        // Read a character
+        c = getchar();
+
+        // If we hit EOF, replace it with a null character and return.
+        if(c == EOF || c == '\n')
+        {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+        position++;
+            // If we have exceeded the buffer, reallocate.
+    if(position >= bufsize){
+        bufsize += LSH_RL_BUFSIZE;
+        buffer = realloc(buffer, bufsize);
+        if(!buffer) {
+            fprintf(stderr, "lsh: allocation error \n");
+            exit(EXIT_FAILURE);
+            }
+        }
+    }
+    char *line = NULL;
+    ssize_t bufsize = 0; //have get line allocate a buffer for us
+    getline(&line, &bufsize, stdin);
+    return line;
+}
+
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+char **lsh_split_line(char *line)
+{
+    int bufsize = LSH_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if(!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+    token = strtok(line, LSH_TOK_DELIM);
+    while(token != NULL){
+        tokens[position] = token;
+        position++;
+
+        if(position >= bufsize){
+            bufsize += LSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if(!tokens){
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, LSH_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
